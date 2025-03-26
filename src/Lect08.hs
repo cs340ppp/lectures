773f24c8baf1ea0a055a4666c39fc3143c86401e
Michael Lee <lee@soi2.org>
2025-03-26 09:43:39 -0500

    Removing commentary

---
+++ src/Lect08.hs
@@ -0,0 +1,172 @@
+module Lect08 where
+import Prelude hiding (Word, Maybe, Just, Nothing, Either, Left, Right)
+import Data.Char
+
+-- Type synonyms: English terms 
+
+type Letter = Char
+type Word = [Letter]
+type Sentence = [Word]
+
+phrases :: [Word] -> [Word] -> [Sentence]
+phrases adjs nouns = [ [adj, noun]
+                       | adj <- adjs, noun <- nouns ]
+
+
+-- Type synonyms: tuple aliases
+
+type Point2D = (Double, Double)
+
+distance :: Point2D -> Point2D -> Double
+distance (x1,y1) (x2,y2) = sqrt $ (x1-x2)^2 + (y1-y2)^2
+
+
+type Numbered a = (Int, a)
+
+enumerate :: [a] -> [Numbered a]
+enumerate xs = zip [1..] xs
+
+-- Algebraic data types
+
+data YesOrNo = Yes | No deriving Show  -- "deriving Show" automates String conversion
+
+not' :: YesOrNo -> YesOrNo
+not' Yes = No
+not' No  = Yes
+
+(|||) :: YesOrNo -> YesOrNo -> YesOrNo
+No ||| No = No
+_  ||| _  = Yes
+
+data Box = Box Int Bool String deriving Show
+
+b1 = Box 5 True "hello"
+b2 = Box 100 False "goodbye"
+
+boxAdd :: Box -> Box -> Box
+boxAdd (Box i1 b1 s1) (Box i2 b2 s2)
+       = Box (i1 + i2) (b1 || b2) (s1 ++ s2)
+
+data Shape = Circle Double 
+             | Triangle Double Double 
+             | Rectangle Double Double deriving Show
+
+area :: Shape -> Double
+area (Circle r)      = pi * r^2
+area (Triangle h b)  = (h*b)/2
+area (Rectangle l w) = l*w
+
+-- Counting values (sums & products)
+
+data T1 = T1V1 | T1V2 | T1V3
+data T2 = T2V1 Bool | T2V2 T1
+data T3 = T3V Bool T1
+data T4 = T4V1 T1 T2 | T4V2 T2 T3
+
+-- Record notation
+
+data Student = Student { firstName :: String
+                       , lastName  :: String
+                       , studentId :: Integer
+                       , grades    :: [Char]
+                       } deriving Show
+
+s1 = Student "John" "Doe" 1234 ['A', 'B']
+
+s2 = Student { lastName = "Doe"
+             , firstName = "Jane"
+             , grades = ['A', 'C']
+             , studentId = 2345 }
+
+s3 = s1 { grades = ['B', 'A', 'D'] }
+
+-- Self-referential types
+
+data RussianDoll = Doll String RussianDoll | EmptyDoll deriving Show
+
+d1 = EmptyDoll
+d2 = Doll "privyet" EmptyDoll
+d3 = Doll "matry" (Doll "osh" (Doll "ka" EmptyDoll))
+d4 = Doll "infinity, and beyond!" d4
+
+
+innerMostMessage :: RussianDoll -> String
+innerMostMessage EmptyDoll          = error "No message"
+innerMostMessage (Doll m EmptyDoll) = m
+innerMostMessage (Doll _ d)         = innerMostMessage d
+
+-- Parametric types
+
+data UBox a = Box' a deriving Show
+
+ub1 :: UBox Bool
+ub1 = Box' True
+
+ub2 :: UBox [Int]
+ub2 = Box' [1..10]
+
+ub3 :: UBox (Int -> Bool)
+ub3 = Box' even
+
+mapBox :: (a -> b) -> UBox a -> UBox b
+mapBox f (Box' x) = Box' $ f x
+
+sumBoxes :: Num a => [UBox a] -> UBox a
+sumBoxes [] = Box' 0
+sumBoxes (Box' n : bs) = let Box' ns = sumBoxes bs 
+                        in Box' $ n + ns
+
+-- kinds of types
+             
+data Bar a b = Bar1 a | Bar2 b
+data Bat a b c = Bat a b c
+data Baz a b = Baz (a b)
+
+-- Some useful parametric types
+
+data Maybe a = Just a | Nothing deriving Show
+
+quadRoots :: Double -> Double -> Double -> (Double,Double)
+quadRoots a b c = let d = b^2-4*a*c
+                      sd = sqrt d
+                  in if d < 0
+                     then error "No real roots"
+                     else ((-b+sd)/(2*a), (-b-sd)/(2*a))
+
+
+find :: (a -> Bool) -> [a] -> a
+find _ [] = error "Value not found"
+find p (x:xs) | p x = x
+              | otherwise = find p xs
+
+data Either a b = Left a | Right b deriving Show
+
+find' :: (a -> Bool) -> [a] -> a
+find' _ [] = error "List was empty"
+find' p (x:xs) | p x = x
+               | null xs = error "No element satisifying predicate"
+               | otherwise = find' p xs
+
+infixr 5 :-
+data List a = a :- (List a) | Null deriving Show
+
+l1 :: List Char
+l1 = 'h' :- 'e' :- 'l' :- 'l' :- 'o' :- Null
+
+l2 :: List Char
+l2 = 'h' :- 'a' :- Null
+
+l3 :: List (List Int)
+l3 = (1 :- 2 :- Null) :- (3 :- 4 :- Null) :- Null
+
+enumFromL :: (Eq a, Enum a) => a -> List a
+enumFromL m = m :- enumFromL (succ m)
+
+takeL :: Int -> List a -> List a
+takeL 0 _    = Null
+takeL _ Null = Null
+takeL n (x :- xs) = x :- takeL (n-1) xs
+
+mapL :: (a -> b) -> List a -> List b
+mapL _ Null = Null
+mapL f (x :- xs) = f x :- mapL f xs
