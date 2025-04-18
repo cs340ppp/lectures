7dfb2f8f3ff08b596a49f0bec324c843dbcfbef7
Michael Lee <lee@soi2.org>
2025-04-18 09:34:48 -0500

    Updating sample code

--- src/Lect12.hs
+++ src/Lect12.hs
@@ -18,19 +18,14 @@ instance (Monad m) => Functor (StateT s m) where
 
 instance (Monad m) => Applicative (StateT s m) where
   pure :: a -> StateT s m a
-  pure x = StateT $ \s -> return (x, s)
+  pure x = undefined
 
   (<*>) :: StateT s m (a -> b) -> StateT s m a -> StateT s m b
-  StateT mf <*> StateT mx = StateT $ \s -> do
-    (f, s') <- mf s
-    (x, s'') <- mx s'
-    return (f x, s'')
+  StateT mf <*> StateT mx = undefined
 
 instance (Monad m) => Monad (StateT s m) where
   (>>=) :: StateT s m a -> (a -> StateT s m b) -> StateT s m b
-  StateT st >>= f = StateT $ \s -> do
-    (x, s') <- st s
-    runStateT (f x) s'
+  StateT st >>= f = undefined
 
 pop :: StateT [a] Maybe a
 pop = StateT $ \s -> case s of
@@ -43,41 +38,6 @@ push x = StateT $ \s -> Just ((), x:s)
 stackOps :: StateT [Int] Maybe ()
 stackOps = push 1 >> pop >> pop >> push 2
 
--- some simple functions to read/write state
-
-get :: Monad m => StateT s m s
-get = StateT $ \s -> return (s, s)
-
-put :: Monad m => s -> StateT s m ()
-put s = StateT $ \_ -> return ((), s)
-
--- lift a value from the base monad into StateT, threading the state through
-
-lift :: Monad m => m a -> StateT s m a
-lift m = StateT $ \s -> do x <- m
-                           return (x, s)
-
--- updated guessing game
-
-guess :: StateT (Int,Int) IO ()
-guess = do (target,tries) <- get
-           lift $ putStrLn "Enter a guess"
-           input <- lift getLine
-           case reads input of -- reads tries to parse input for Int
-             [(guessVal, "")] -> do
-               put (target, tries+1)
-               if guessVal < target then do
-                 lift $ putStrLn "Too low!"
-                 guess
-               else if guessVal > target then do
-                 lift $ putStrLn "Too high!"
-                 guess
-               else
-                 lift $ putStrLn $ "You got it in "++show tries++" tries"
-             _ -> do
-               lift $ putStrLn "Invalid input."
-               guess 
-           
 -- Parsing with the State monad
 
 type Parser a = StateT String Maybe a
@@ -145,15 +105,8 @@ expr = do t1 <- term
        term
 
 term :: Parser Expr
-term = do symbol "("
-          e <- expr
-          symbol ")"
-          return e
-       <|>
-       do i <- token int
-          return $ Lit i
+term = undefined
           
-
 eval :: Expr -> Int
 eval (Lit i) = i
 eval (Par e) = eval e
@@ -161,6 +114,40 @@ eval (Add e1 e2) = eval e1 + eval e2
 eval (Sub e1 e2) = eval e1 - eval e2
 
 evalString :: String -> Maybe Int
-evalString s = do
-  (e, _) <- runStateT expr s
-  return $ eval e
+evalString s = undefined
+
+-- some simple functions to read/write state
+
+get :: Monad m => StateT s m s
+get = StateT $ \s -> return (s, s)
+
+put :: Monad m => s -> StateT s m ()
+put s = StateT $ \_ -> return ((), s)
+
+-- lift a value from the base monad into StateT, threading the state through
+
+lift :: Monad m => m a -> StateT s m a
+lift m = StateT $ \s -> do x <- m
+                           return (x, s)
+
+-- updated guessing game
+
+guess :: StateT (Int,Int) IO ()
+guess = do (target,tries) <- get
+           lift $ putStrLn "Enter a guess"
+           input <- lift getLine
+           case reads input of -- reads tries to parse input for Int
+             [(guessVal, "")] -> do
+               put (target, tries+1)
+               if guessVal < target then do
+                 lift $ putStrLn "Too low!"
+                 guess
+               else if guessVal > target then do
+                 lift $ putStrLn "Too high!"
+                 guess
+               else
+                 lift $ putStrLn $ "You got it in "++show tries++" tries"
+             _ -> do
+               lift $ putStrLn "Invalid input."
+               guess 
+           
