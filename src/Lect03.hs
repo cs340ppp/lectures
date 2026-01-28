1790dd039c58252b64607571126be8f0782a8e1e
Michael Lee <lee@soi2.org>
2026-01-27 18:50:02 -0600

    Updating function lecture source.

--- src/Lect03.hs
+++ src/Lect03.hs
@@ -1,51 +1,69 @@
 module Lect03 where
 import Data.Char
 
+inc = \x -> x + 1
+
+incForever = \x -> incForever x + 1
+
 nand :: Bool -> Bool -> Bool
 nand x y = not (x && y)
 
-distance :: (Floating a) => (a, a) -> (a, a) -> a
-distance p1 p2 = sqrt ((fst p1 - fst p2)^2 + (snd p1 - snd p2)^2)
+c2f :: (Floating a) => a -> a
+c2f c = c * 9 / 5 + 32
+
+(<<) :: (Ord a, Num a) => a -> a -> Bool
+x << y = x*10 < y
+
+infixl 6 +++
+(+++) :: String -> String -> String
+s1 +++ s2 = s1 ++ ":::" ++ s2
+
+infixl 7 ***
+(***) :: Char -> Int -> String
+c *** n = replicate n c
 
 not' :: Bool -> Bool
-not' True = False
+not' True  = False
 not' False = True
-
 
 fib :: Integer -> Integer
 fib 0 = 0
 fib 1 = 1
 fib n = fib (n-1) + fib (n-2)
 
-
 nand' :: Bool -> Bool -> Bool
 nand' False False = True
 nand' _ _ = False
 
+fst' :: (a, b) -> a
+fst' (x, _) = x
 
-fst' :: (a,b) -> a
-fst' (x,_) = x
+snd' :: (a, b) -> b
+snd' (_, y) = y
 
-snd' :: (a,b) -> b
-snd' (_,y) = y
-
+distance :: (Floating a) => (a, a) -> (a, a) -> a
+distance p1 p2 = sqrt ((fst p1 - fst p2)^2 + (snd p1 - snd p2)^2)
 
 distance' :: (Floating a) => (a, a) -> (a, a) -> a
-distance' (x1,y1) (x2,y2) = sqrt ((x1-x2)^2 + (y1-y2)^2)
+distance' (x1, y1) (x2, y2) = sqrt ((x2 - x1)^2 + (y2 - y1)^2)
 
+distanceFromOrigin :: (Floating a) => (a, a) -> a
+distanceFromOrigin = distance (0,0)
 
-mapTup :: (a -> b) -> (a, a) -> (b, b)
-mapTup f (x,y) = (f x, f y)
+classifyPoint :: (Int, Int) -> String
+classifyPoint (0, 0) = "origin"
+classifyPoint (_, 0) = "x-axis"
+classifyPoint (0, _) = "y-axis"
+classifyPoint _      = "off-axis"
 
+get3 :: Int -> (a, a, a) -> a
+get3 0 (x, _, _) = x
+get3 1 (_, y, _) = y
+get3 2 (_, _, z) = z
+get3 _ _         = error "get3: index out of bounds"
 
-foo :: (a, (b, c)) -> ((a, (b, c)), (b, c), (a, b, c))
-foo p@(x, q@(y, z)) = (p, q, (x, y, z))
-
-
-fib' :: Integer -> Integer
-fib' n | n == 0 = 0
-       | n == 1 = 1
-       | otherwise = fib' (n-1) + fib' (n-2)
+pointWithDistance :: (Floating a) => (a, a) -> ((a, a), a)
+pointWithDistance p@(x, y) = (p, sqrt (x^2 + y^2))
 
 letterGrade :: (Ord a, Num a) => a -> Char
 letterGrade n | n >= 90   = 'A'
@@ -53,38 +71,21 @@ letterGrade n | n >= 90   = 'A'
               | n >= 70   = 'C'
               | n >= 60   = 'D'
               | otherwise = 'E'
-c2f :: (Floating a) => a -> a
-c2f c = c * 9 / 5 + 32
 
 c2h :: (Floating a, Ord a) => a -> String
-c2h c | f >= 100 = "hot"
-      | f >= 70  = "comfortable"
-      | f >= 50  = "cool"
-      | otherwise    = "cold"
-      where f = c2f c
+c2h c | f >= 100   = "hot"
+      | f >= 70    = "comfortable"
+      | f >= 50    = "cool"
+      | otherwise  = "cold"
+  where f = c2f c
 
-closer :: (Floating a, Ord a) => (a, a) -> (a, a) -> (a, a) -> (a, a)
-closer src dst1 dst2 = if d1 < d2 then dst1 else dst2
-  where d1 = distance src dst1
-        d2 = distance src dst2
-
-
-quadrant :: (Int, Int) -> Int
-quadrant (x, y) | x > 0 && y > 0 = 1
-                | x < 0 && y > 0 = 2
-                | x < 0 && y < 0 = 3
-                | x > 0 && y < 0 = 4
-                | otherwise      = 0
-
-quadrantNames :: (Int, Int) -> String
-quadrantNames (x, y) = case quadrant (x, y) of 1 -> "All"
-                                               2 -> "Science"
-                                               3 -> "Teachers"
-                                               4 -> "Crazy"
-                                               _ -> "Origin"
+c2fishing c = "Today is a "
+         ++ (if f > 60 && f < 90 then "nice" else "bad")
+         ++ " day for fishing."
+  where f = c2f c
 
 verboseCompare :: Int -> Int -> String
-verboseCompare x y = 
+verboseCompare x y =
   "Comparing " ++ show x ++ " and " ++ show y ++ ": " ++
   case (abs x, abs y) of
     (0, 0) -> "both zero"
@@ -95,7 +96,17 @@ verboseCompare x y =
            | otherwise -> "equal magnitudes"
 
 quadRoots :: Double -> Double -> Double -> (Double, Double)
-quadRoots a b c = let disc = a^2 - 4*a*c
-                      x1 = (-b + sqrt disc) / (2*a)
-                      x2 = (-b - sqrt disc) / (2*a)
-                  in (x1, x2)
+quadRoots a b c = let discriminant = b^2 - 4*a*c
+                      sqrtDisc = sqrt discriminant
+                  in ((-b + sqrtDisc) / 2*a, (-b - sqrtDisc) / 2*a)
+
+bmi weight height
+  | bmi < 18.5 = "underweight"
+  | bmi < 25.0 = "normal"
+  | bmi < 30.0 = "overweight"
+  | otherwise  = "obese"
+  where bmi = weight / height^2
+
+cylinderVolume r h = 
+  let baseArea = pi * r^2
+  in baseArea * h
